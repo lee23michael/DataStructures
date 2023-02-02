e06dc265704e79da54e71007edf6cd37eb906d3b
lee23michael <lee23michael@gmail.com>
2023-02-02 14:22:42 -0800

    Add files via upload

--- Maps/SortedMap/src/Main.java
+++ Maps/SortedMap/src/Main.java
@@ -1,7 +1,37 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        Map<Integer, String> hashMap = new HashMap<Integer, String>();
+        Map<Integer, String> linkedHashMap = new LinkedHashMap<Integer, String>();
+        Map<Integer, String> treeMap = new TreeMap<Integer, String>();
+
+        testMap(hashMap);
+        testMap(linkedHashMap);
+        testMap(treeMap);
+    }
+
+    public static void testMap(Map<Integer, String> map)
+    {
+        map.put(9,"nine");
+        map.put(4,"four");
+        map.put(23,"twenty three");
+        map.put(11,"eleven");
+        map.put(0,"zero");
+        map.put(19,"nineteen");
+
+        for(Integer key: map.keySet())
+        {
+            String value = map.get(key);
+
+            System.out.println(key + ":" + value);
+        }
+
+        System.out.println("\n");
     }
 }
