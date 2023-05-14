75ab8fe9031955464f32d398682e05f3748f2aae
lee23michael <lee23michael@gmail.com>
2023-05-13 21:10:21 -0700

    Add files via upload

---
+++ TruncateThecharacterWithSubscript.java
@@ -0,0 +1,17 @@
+package src;
+
+import java.util.Scanner;
+
+public class TruncateThecharacterWithSubscript
+{
+    public static void main(String[] args)
+    {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+       System.out.println(input.charAt(5));
+
+    }
+
+}
